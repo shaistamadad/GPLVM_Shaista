b7e86bc5ed3394e2b1fa32b616157c9109538e11
Emma Dann <ed6@sanger.ac.uk>
2021-11-15 16:05:07 +0000

    added argparse minimal example

---
+++ example_scripts/argparse_example.py
@@ -0,0 +1,25 @@
+## Example python script using argparse 
+# (here I read an .h5ad object and rename the column storing the celltype information)
+import numpy as np
+import pandas as pd
+import scanpy
+
+import argparse
+parser = argparse.ArgumentParser()
+## Example required argument
+parser.add_argument("filepath", help="path to anndata file")
+## Example optional argument
+parser.add_argument("--celltype_column", 
+                    default="celltype",
+                    help="string indicating column in adata.obs storing cell type information")
+args = parser.parse_args()
+
+## Parse arguments
+h5ad_file = args.filepath
+celltype_col = args.celltype_column
+
+## Your script starts here ##
+adata = sc.read_h5ad(filepath)
+adata.obs['annotation'] = adata.obs[celltype_col]
+output_filepath = filepath.split('.h5ad')[0] + ".cleaned.h5ad"
+sc.write_h5ad(output_filepath)
